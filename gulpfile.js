438193f974095176dbd27f02506a96b28caf4590
Timothy Stepp <timstepp@me.com>
2016-09-21 02:28:43 -0500

    minify assets

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,22 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var pump = require('pump');
+var cleanCSS = require('gulp-clean-css');
+
+gulp.task('minify-css', function() {
+  return gulp.src('app/assets/css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('public/css'));
+});
 
 gulp.task('compress', function (cb) {
   pump([
-        gulp.src('app/assets/*.js'),
+        gulp.src('app/assets/js/*.js'),
         uglify(),
         gulp.dest('public/js')
     ],
     cb
   );
+
+
 });
